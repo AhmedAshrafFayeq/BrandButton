97e101f74c1d0b0824e6de7682b6ccae04832138
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2024-03-05 01:00:23 +0300

    Add example for BrandButton

--- Example/BrandButton/ViewController.swift
+++ Example/BrandButton/ViewController.swift
@@ -11,48 +11,100 @@ import BrandButton
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var brandButton: CustomBrandButton!
-    @IBOutlet weak var blueBrandButton: CustomBrandButton!
-    @IBOutlet weak var secondaryGreenButton: CustomBrandButton!
-    @IBOutlet weak var secondaryBlueButton: CustomBrandButton!
+    // MARK: - Properties
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    var typeSegmentedControl: UISegmentedControl!
+    var colorStyleSegmentedControl: UISegmentedControl!
+    var iconStyleSegmentedControl: UISegmentedControl!
+    var isEnabledSwitch: UISwitch!
+    let brandButton = CustomBrandButton()
+    
+    let image = UIImage(named: "swift")
+    let highlightedImage = UIImage(named: "swift-2")
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        configureUI()
+        configureButton()
+    }
+    
+    // MARK: - UI Configuration
+    
+    private func configureUI() {
+        typeSegmentedControl = createSegmentedControl(withOptions: ["Primary", "Secondary"])
+        typeSegmentedControl.center = CGPoint(x: view.center.x, y: view.center.y - 230)
+        view.addSubview(typeSegmentedControl)
+        
+        colorStyleSegmentedControl = createSegmentedControl(withOptions: ["Green", "Blue"])
+        colorStyleSegmentedControl.center = CGPoint(x: view.center.x, y: view.center.y - 180)
+        view.addSubview(colorStyleSegmentedControl)
+        
+        iconStyleSegmentedControl = createSegmentedControl(withOptions: ["None", "Leading", "Trailing"])
+        iconStyleSegmentedControl.center = CGPoint(x: view.center.x, y: view.center.y - 130)
+        view.addSubview(iconStyleSegmentedControl)
+        
+        isEnabledSwitch = UISwitch()
+        isEnabledSwitch.center = CGPoint(x: view.center.x, y: view.center.y - 80)
+        isEnabledSwitch.isOn = true
+        isEnabledSwitch.addTarget(self, action: #selector(switchValueChanged(_:)), for: .valueChanged)
+        view.addSubview(isEnabledSwitch)
+    }
+    
+    private func configureButton() {
+        brandButton.frame = CGRect(x: 0, y: 0, width: 220, height: 45)
+        brandButton.center = view.center
+        view.addSubview(brandButton)
         brandButton.addTarget(self, action: #selector(didTapBrandButton), for: .touchUpInside)
-        brandButton.leadingIcon = UIImage(named: "swift")
-        brandButton.font = UIFont(name: "Helvetica-Regular", size: 15)
-        brandButton.highlitedFont = UIFont(name: "Helvetica-Bold", size: 18)
-        brandButton.highlitedLeadingIcon = UIImage(named: "swift-2")
         
-        blueBrandButton.title = "Primary Blue"
-        blueBrandButton.colorStyle = .blue
-//        blueBrandButton.trailingIcon = UIImage(named: "swift")
-        
-        secondaryGreenButton.title = "Secondary"
-        secondaryGreenButton.type = .secondary
-        secondaryGreenButton.leadingIcon = UIImage(named: "swift-2")
-        secondaryGreenButton.addTarget(self, action: #selector(didTapsecondaryGreenButton), for: .touchUpInside)
-        
-        secondaryBlueButton.title = "Secondary Blue"
-        secondaryBlueButton.type = .secondary
-        secondaryBlueButton.colorStyle = .blue
-        secondaryBlueButton.isEnabled = false
-        secondaryBlueButton.leadingIcon = UIImage(named: "swift-2")
-        
+        // Set default configuration
+        brandButton.highlitedFont = UIFont(name: "Helvetica-Bold", size: 20)
+        brandButton.isEnabled = true
+        brandButton.type = .primary
+        brandButton.colorStyle = .green
     }
     
-    @objc func didTapBrandButton() {
-        print("did Tap Brand button")
-        blueBrandButton.leadingIcon = UIImage(named: "swift-2")
+    private func createSegmentedControl(withOptions options: [String]) -> UISegmentedControl {
+        let segmentedControl = UISegmentedControl(items: options)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        return segmentedControl
     }
     
-    @objc func didTapsecondaryGreenButton() {
-        print("did Tap Brand button")
-        blueBrandButton.trailingIcon = UIImage(named: "swift")
-        blueBrandButton.highlitedTrailingIcon = UIImage(named: "swift-2")
-
-        secondaryBlueButton.isEnabled = true
-
+    // MARK: - Actions
+    
+    @objc private func switchValueChanged(_ sender: UISwitch) {
+        brandButton.isEnabled = sender.isOn
+    }
+    
+    @objc private func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        switch sender {
+        case typeSegmentedControl:
+            brandButton.type = (sender.selectedSegmentIndex == 0) ? .primary : .secondary
+        case colorStyleSegmentedControl:
+            brandButton.colorStyle = (sender.selectedSegmentIndex == 0) ? .green : .blue
+        case iconStyleSegmentedControl:
+            switch sender.selectedSegmentIndex {
+            case 0:
+                brandButton.leadingIcon = nil
+                brandButton.trailingIcon = nil
+            case 1:
+                brandButton.leadingIcon = image
+                brandButton.highlitedLeadingIcon = highlightedImage
+            case 2:
+                brandButton.trailingIcon = image
+                brandButton.highlitedTrailingIcon = highlightedImage
+            default:
+                break
+            }
+        default:
+            break
+        }
+    }
+    
+    @objc private func didTapBrandButton() {
+        print("Brand button tapped")
     }
 }
